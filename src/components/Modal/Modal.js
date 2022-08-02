44a117133807151189b85b624656ade5d6848b6d
Mike Bakuzov <bakuzov@gmail.com>
2022-08-02 23:07:27 +0300

    +

---
+++ src/components/Modal/Modal.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { createPortal } from 'react-dom';
+// import styles from './Modal.module.css';
+
+const modalRoot = document.querySelector('#modal-root');
+
+export class Modal extends Component {
+  static propTypes = {
+    close: PropTypes.func.isRequired,
+  };
+
+  componentDidMount() {
+    window.addEventListener('keydown', this.handClickDown);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('keydown', this.handClickDown);
+  }
+
+  handClickDown = e => {
+    if (e.code === 'Escape') {
+      this.props.close();
+    }
+  };
+
+  handleClickOnOverlay = evt => {
+    if (evt.target === evt.currentTarget) {
+      this.props.close();
+    }
+  };
+
+  render() {
+    const { largeImageURL, close } = this.props;
+    return createPortal(
+      <div onClick={this.handleClickOnOverlay}>
+        <div>
+          <img src={largeImageURL} alt="краса" onClick={close} />
+        </div>
+      </div>,
+      modalRoot
+    );
+  }
+}
